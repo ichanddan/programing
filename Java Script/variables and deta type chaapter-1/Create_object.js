b5f8cbb3fa73753fe85f8490affd3d6365545bb5
ichanddan <code.us.cm@gmail.com>
2024-02-10 11:46:47 +0530

    js Create object

---
+++ Java Script/variables and deta type chaapter-1/Create_object.js
@@ -0,0 +1,12 @@
+const student ={
+    fullName:"chandan Kumar Maurya",
+    age:24,
+    cgpa:6.1,
+    isPass:true
+};
+// change valu in obejct using variable.veriable = valu using . or ['enter variable name']
+student.age=student.age+1
+console.log(student);
+console.log(typeof student);
+console.log(student.fullName);
+console.log(student['age']);
