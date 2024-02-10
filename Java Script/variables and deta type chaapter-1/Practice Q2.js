8fa05cb5cc1965f0c02813b2fc773975eba9b1ec
ichanddan <code.us.cm@gmail.com>
2024-02-10 12:42:58 +0530

    js practice q2

---
+++ Java Script/variables and deta type chaapter-1/Practice Q2.js
@@ -0,0 +1,12 @@
+const profile={
+    Name:'SoftUity',
+    userName:'Softuity',
+    post:3,
+    isFollowers:true,
+    isFollowing:3,
+    tageLine:'SoftUity Provided Web development, UIUX, WordPress development and full stack web development service',
+    bio:'SoftUity is a web development company providing web development, UIUX, WordPress development and full stack web development service',
+    image:'https://www.softuity.com/wp-content/uploads/2020/03/softuity-logo-1.png',
+};
+delete profile.bio;
+console.log(profile);
