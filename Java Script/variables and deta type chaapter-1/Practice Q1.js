6d8e35eaabaa30592b76e81fbbf369fc31326ad9
ichanddan <code.us.cm@gmail.com>
2024-02-10 11:58:41 +0530

    js practice q1

---
+++ Java Script/variables and deta type chaapter-1/Practice Q1.js
@@ -0,0 +1,8 @@
+const Product={
+    Name:'Parker jotter Standerd CT bell Pen (Black)',
+    Rating:'4/5',
+    Deal: 'Deal of the day',
+    Price:270 ,
+};
+console.log(Product);
+console.log(typeof Product);
