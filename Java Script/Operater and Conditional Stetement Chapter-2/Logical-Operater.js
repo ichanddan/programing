559de11bb399bcc7ce21e1c3172ca574574e2557
ichanddan <code.us.cm@gmail.com>
2024-02-11 15:22:47 +0530

    Logical-Operater

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Logical-Operater.js
@@ -0,0 +1,17 @@
+// Logical Operater 
+// AND Operater
+// OR Operater
+// NOT Operater
+
+let a,b;
+a=10;
+b=20;
+
+let cond1=a<b;
+let cond2=a===10;
+console.log('cond1 && cond2', cond1 && cond2); // if using AND operater any valu are false valu will be return false booth of valu true will be return true
+let cond3 = a>b;
+console.log('cond1 || cond3', cond1 || cond3); // if using OR oprater any valu true is return valu is true betwin 2 two valu
+console.log(' !(a<b)', !(cond1<cond3));// if valu is true so return false and valu is false return true
+
+
