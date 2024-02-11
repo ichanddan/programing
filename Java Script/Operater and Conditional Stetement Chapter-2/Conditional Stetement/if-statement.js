7778c80c1b16e587d549fb0747c87f7c1ea82a99
ichanddan <code.us.cm@gmail.com>
2024-02-11 22:12:08 +0530

    If Statement

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Conditional Stetement/if-statement.js
@@ -0,0 +1,17 @@
+let age=15;
+if (age>=18) {
+    console.log("You can vote your age is",age);
+}
+if (age<18) {
+    console.log("You can't vote your age is",age);
+}
+
+let mode="Dark";
+let color;
+if (mode ==="Dark") {
+    color = "black"
+}
+if (mode === "light") {
+    color="White"
+}
+console.log(color)
