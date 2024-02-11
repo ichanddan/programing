aaaaf0685e2709b1c63ec1d8e0a7cfb4bee9ac88
ichanddan <code.us.cm@gmail.com>
2024-02-12 01:04:57 +0530

    operater and conditional statement using practice q1

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Practice Q1.js
@@ -0,0 +1,7 @@
+let valu=prompt("Enter the number");
+if (valu %5===0) {
+    console.log("this is divided by 5 number",valu);
+
+} else{
+    console.log("this is not divided by 5 number",valu);
+}
