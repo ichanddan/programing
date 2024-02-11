c809d477073e04d2d3a04bff25223bd89375e91f
ichanddan <code.us.cm@gmail.com>
2024-02-11 22:36:36 +0530

    else if statement

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Conditional Stetement/Prectice Q1.js
@@ -0,0 +1,8 @@
+// find the even or odd
+
+let num=10;
+if (num%2==0) {
+    console.log(num,"even");
+} else {
+    console.log(num,"odd");
+}
---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Conditional Stetement/else-if-statement.js
@@ -0,0 +1,12 @@
+let mode="red";
+let color;
+if (mode==="Dark") {
+    color = "black"
+} else if(mode==="red"){
+    color="red"
+} else if(mode==="blue"){
+    color="blue"
+} else {
+    color="White"
+}
+console.log(color);
