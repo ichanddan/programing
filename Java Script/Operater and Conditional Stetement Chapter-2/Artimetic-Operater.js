d68e693dc0896b243627697ffa6bbb02d9dd6166
ichanddan <code.us.cm@gmail.com>
2024-02-11 13:59:03 +0530

    Artimetic Operators

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Artimetic-Operater.js
@@ -0,0 +1,21 @@
+// comments there are 2 type of comments 
+// single line Comment using //
+// multiple line /*your content here*/
+
+
+// Operater there are 3 type of operater 
+// Artimetic operatrs
+// unary Operator
+// bitwase operator
+
+
+// Artimetic operatrs
+let a,b;
+a=10; b=3;
+console.log('a+b',a+b)
+console.log('a-b',a-b)
+console.log('a*b',a*b)
+console.log('a/b',a/b)
+console.log('a%b',a%b) /*give you devided riminder*/
+console.log('a**b',a**b)
+console.log('a++',a++)
