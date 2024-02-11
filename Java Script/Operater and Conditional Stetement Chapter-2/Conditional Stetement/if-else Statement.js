231ccf0d88f3ac5086a2fb32a50aa11a6e45b9e6
ichanddan <code.us.cm@gmail.com>
2024-02-11 22:23:38 +0530

    If else conditional

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Conditional Stetement/if-else Statement.js
@@ -0,0 +1,15 @@
+let body="black"
+if (body==="black") {
+    console.log("body is black");
+} else {
+    console.log("body color is white")
+}
+
+let Dark="black"
+let color;
+if (Dark==="black") {
+    color = "black"
+} else {
+    color="White"
+}
+console.log(color);
