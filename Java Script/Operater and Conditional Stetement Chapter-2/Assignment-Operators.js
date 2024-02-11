9ad278944b89c63eb0f68e798945b3c1f2b071dc
ichanddan <code.us.cm@gmail.com>
2024-02-11 14:38:15 +0530

    Artimetic-Operater

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Assignment-Operators.js
@@ -0,0 +1,19 @@
+// assingment operators
+
+
+
+
+let a=2;
+let b=3;
+a+=4 /*is = to a=a+4*/
+console.log('a=a+4 using sort +=',a); /* output program old valu of a is 2 and a=a+4 using += operators valu is 6*/
+a-=2;
+console.log('a=a-6 using sort -=',a); /* output program old valu of a is 6 and a=a-2 using -= operators valu is 4*/
+a*=2;
+console.log('a=a*2 using sort *=',a); /* output program old valu of a is 4 and a=a*2 using *= operators valu is 8*/
+a/=2;
+console.log('a=a/2 using sort /=',a); /* output program old valu of a is 8 and a=a/2 using /= operators valu is 4*/
+a%=2;
+console.log('a=a%2 using sort %=',a); /* output program old valu of a is 4 and a=a%2 using %= operators valu is 0*/
+b**=5;
+console.log('b=b**5 using sort **=',b); /* output program old valu of b is 3 and b=b**5 using **= operators valu is 243*/
