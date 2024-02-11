8e3ce4821e5503c9e7487deff15d98e1a1e9504b
ichanddan <code.us.cm@gmail.com>
2024-02-11 14:55:30 +0530

    Comparsion-Operater

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/Comparsion-Operater.js
@@ -0,0 +1,23 @@
+// comparsion operators
+// equal to using duble eqalto ==
+// cheek Equal valu to & type ===
+// not equal to using duble not equal to!=
+// less than using duble less than <
+// less than or equal to using duble less than or equal to <=
+// greater than using duble greater than >
+// greater than or equal to using duble greater than or equal to >=
+// not equal to & type !==
+// not equal to !=
+
+let a=2;
+let b=3;
+console.log("2==3",a==b); /*return boolen valu allways comparer betwin two valu*/
+console.log("2!=3",a!=b); /*return boolen valu allways comparer betwin two valu*/
+console.log("2<3",a<b); /*return boolen valu allways comparer betwin two valu*/
+console.log("2<=3",a<=b); /*return boolen valu allways comparer betwin two valu*/
+console.log("2>3",a>b); /*return boolen valu allways comparer betwin two valu*/
+console.log("2>=3",a>=b); /*return boolen valu allways comparer betwin two valu*/
+console.log("2===3",a===b); /*return boolen valu allways comparer betwin two valu*/
+let c=5;
+let d=5;
+console.log("5===5",c===d); /*return boolen valu allways comparer betwin two valu*/
