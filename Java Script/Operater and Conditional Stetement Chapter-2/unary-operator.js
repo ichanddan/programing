e1b46aa0228d06c68af010904338d118a9ea9beb
ichanddan <code.us.cm@gmail.com>
2024-02-11 14:18:02 +0530

    Unary Operator

---
+++ Java Script/Operater and Conditional Stetement Chapter-2/unary-operator.js
@@ -0,0 +1,20 @@
+// unary operator ther are 2 type of unary operator
+// 1 pre increment
+// 2 post increment 
+// 2 pre decrement
+// 3 post decrement
+
+
+let a=1;
+let b=2;
+
+
+
+
+console.log('a=',a,'b=',b);
+console.log('a++',a++);
+console.log('a=',a)
+console.log('b--',b--);
+console.log('b=',b)
+console.log('++a',++a);
+console.log('--a',--b);
