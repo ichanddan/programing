a5b9853f170de6c51bf56b95755da19ab4a812fd
ichanddan <code.us.cm@gmail.com>
2024-02-13 04:05:45 +0530

    for in loop

---
+++ Java Script/loop and string/loop/for in loop.js
@@ -0,0 +1,13 @@
+let student={
+    name:"NAME",
+    age:20,
+    gender:"male",
+    city:"Bangalore"
+
+}
+for(let i in student){
+    console.log(i);
+    console.log(student[i]);
+   
+}
+// for in loop using with onject
