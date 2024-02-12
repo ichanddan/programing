0fc2b99ab36a6f37aba67f6a0d86260c9e2c67f0
ichanddan <code.us.cm@gmail.com>
2024-02-13 03:17:26 +0530

    for loop

---
+++ Java Script/loop and string/loop/for loop.js
@@ -0,0 +1,14 @@
+// loop are used to execute a piece of code again & again
+
+let vaul=1;
+for(let i=1; i<=vaul*vaul; i++){
+    console.log(i)
+}
+console.log("loop end");
+
+
+let sum=0;
+for (let index = 1; index <= 5; index++) {
+    sum=sum +index;
+}
+console.log(sum);
