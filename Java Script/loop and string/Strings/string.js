9e59b248551fe63441cb50e5bc45b2d8c96f0843
ichanddan <code.us.cm@gmail.com>
2024-02-16 15:35:04 +0530

    js templete litler

---
+++ Java Script/loop and string/Strings/string.js
@@ -0,0 +1,15 @@
+// let a='this is string';
+// let b="this is string";
+
+// //tampleate letler
+
+// let c=`this is string`;
+// console.log(a==c);
+let oja = {
+    name: 'chandan',
+    age: 23,
+    city: 'konya'
+};
+let output=`what is your name ${oja.name}`;
+console.log(output);
+console.log(`what is your name ${oja.age}`)
