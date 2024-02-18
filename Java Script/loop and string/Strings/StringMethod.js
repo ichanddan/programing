b4217660e16fd2ad54b9441f6ce50105daf03899
ichanddan <code.us.cm@gmail.com>
2024-02-18 23:44:43 +0530

    js slice method

--- Java Script/loop and string/Strings/StringMethod.js
+++ Java Script/loop and string/Strings/StringMethod.js
@@ -7,3 +7,12 @@ console.log(a);
 console.log(b);
 a=a.trim();
 console.log(a);
+
+
+//slice method
+let c='chadand kumar maurya';
+console.log(c.slice(0,6));
+console.log(c.slice(1,6));
+console.log(c.slice(1,5));
+console.log(c.slice(3,4));
+console.log(c.slice(1,3));
