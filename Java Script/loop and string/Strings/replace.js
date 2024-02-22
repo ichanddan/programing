3e38db985a4d435ce7886b0dc703d8daa6270a21
ichanddan <code.us.cm@gmail.com>
2024-02-22 23:16:53 +0530

    js replace method, replaceAll and print value stand index

---
+++ Java Script/loop and string/Strings/replace.js
@@ -0,0 +1,9 @@
+// replace method in js using replace method
+
+let r='Chandan Kumar Maurya';
+console.log(r.replace('Chandan','Aman'));
+
+// print value of index
+
+let name1='Chandan Kumar Maurya';
+console.log(name1.charAt(10));
