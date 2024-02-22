684dcacbf2b171319d306f8d5f3ed286dcf732b3
ichanddan <code.us.cm@gmail.com>
2024-02-22 23:51:14 +0530

    Arrays in js

---
+++ Java Script/Arrays/array.js
@@ -0,0 +1,7 @@
+// arrays typeof is object
+
+
+let marks=[90,24,44,66,55,77,40,70,87,78];
+console.log(marks)
+marks[8]=10;  // change value using index=new value becouse of the array is mutable and string in inmutable can't change index value
+console.log(marks[8])
