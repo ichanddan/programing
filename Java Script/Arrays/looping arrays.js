85d342554c8e920a376eba5342b80bcfb9f15fa2
ichanddan <code.us.cm@gmail.com>
2024-02-23 23:50:55 +0530

    js loop using in array

---
+++ Java Script/Arrays/looping arrays.js
@@ -0,0 +1,7 @@
+// looping over an array 
+let arrays=["chandan",'aman','nitu', 'kingsman'];
+for(let name of arrays){
+    console.log(name.toLocaleUpperCase());
+    console.log(name.length);
+    console.log(typeof name);
+}
