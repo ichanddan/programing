d02d84519f23208143ee9389bb1662e8059c6d01
ichanddan <code.us.cm@gmail.com>
2024-02-26 23:45:56 +0530

    using for loop solve the question Q2

--- Java Script/Arrays/Q2.js
+++ Java Script/Arrays/Q2.js
@@ -1,14 +1,19 @@
 // for given array with prices of 5 items [250,645,300,900,50] all items have a 10% descount and store in valu in array after 10 % descount
-let prices=[250,645,300,900,50];
-// prices[2]=20;
-let idx=0;
-for(let value of prices){
-    console.log(`value at index ${idx}=${value}`);
-    let offer = value/10;
-    prices[idx]= prices[idx] - offer;
-    console.log(`value after offer =${prices[idx]}`);
-    idx++;
-}
-console.log(prices);
+let prices=[250,645,,300,900,50];
+prices[2]=20;
+// let idx=0;
+// for(let value of prices){
+//     console.log(`value at index ${idx}=${value}`);
+//     let offer = value/10;
+//     prices[idx]= prices[idx] - offer;
+//     console.log(`value after offer =${prices[idx]}`);
+//     idx++;
+// }
+// console.log(prices);
 
 // the below code
+for (let i =0; i<prices.length; i++){
+    let offer= prices[i]/10;
+    prices[i]= prices[i] - offer;
+}
+console.log(prices);
