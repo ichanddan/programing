e289c943914cfedca10968a52cfdc9fb9ff510fa
ichanddan <code.us.cm@gmail.com>
2024-02-28 23:52:19 +0530

    js pop method in array

---
+++ Java Script/Arrays/push and pop/pop.js
@@ -0,0 +1,5 @@
+// pop is used for remove data for array
+let arr=[100,500,100, 500];
+arr.pop();
+console.log(arr);
+//output is 100, 500, 100  pop is remove last array 
--- Java Script/Arrays/push and pop/push.js
+++ Java Script/Arrays/push and pop/push.js
@@ -1,3 +1,4 @@
 let array = ["manoj",560,'maurya'];
 array.push('km',60,767675656);
 console.log(array);
+//push is basicaly use for input deta in array
