6516ddc8e82090e5ec5c401b4fb5fd376286572f
ichanddan <code.us.cm@gmail.com>
2024-02-29 21:45:48 +0530

    js splice method in array

---
+++ Java Script/Arrays/Method in array/slice and Splice.js
@@ -0,0 +1,12 @@
+let arr=['tony', 'hulk', 'blackvido','docter dom'];
+console.log(arr);// befor using slice output
+arr=arr.slice(2,3)
+console.log(arr);// affter using slice output
+// allways return new array no changes in old array
+// but splice method is change origenal array
+
+let hero=['tony', 'hulk', 'blackvido','docter dom'];
+console.log(arr);// befor using splice output
+arr=hero.splice(0,2,'man')
+console.log(arr);// affter using splice output
+// allways return new array no changes in old array
