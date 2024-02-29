99be049e7b147fafea23949cb70d60155618ffe5
ichanddan <code.us.cm@gmail.com>
2024-02-29 21:29:54 +0530

    js using array shift method

---
+++ Java Script/Arrays/Method in array/shift.js
@@ -0,0 +1,6 @@
+// shift is using basically remove 1st value in array 
+
+let arr=['tony', 'hulk', 'blackvido'];
+console.log(arr);// befor using shift output
+arr.shift()
+console.log(arr);// affter using shift output
