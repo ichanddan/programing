cd2d0cee6fb370a42b5eda92295a3ac2d18a3d3b
ichanddan <code.us.cm@gmail.com>
2024-02-29 22:09:09 +0530

    update and end array in js

--- Java Script/Arrays/Method in array/Q1.js
+++ Java Script/Arrays/Method in array/Q1.js
@@ -1,3 +1,8 @@
+// Create array to store company=['Bloomberg','microsoft','uber', 'google', 'ibm', 'netflix']
+// a) Remove the first company from array 
+// b) Remove uber & add ola in its place
+// c) Add Amazon at end
+
 let c=['Bloomberg','microsoft','uber', 'google', 'ibm', 'netflix'];
 console.log(c);
 c.shift()
