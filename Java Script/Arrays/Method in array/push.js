20e66f5c293745f82c5c1d4bde2d316128a1e9ff
ichanddan <code.us.cm@gmail.com>
2024-02-29 21:25:27 +0530

    js using unShift method

--- Java Script/Arrays/Method in array/push.js
+++ Java Script/Arrays/Method in array/push.js
@@ -1,4 +1,5 @@
 let array = ["manoj",560,'maurya'];
+console.log(array);
 array.push('km',60,767675656);
 console.log(array);
 //push is basicaly use for input deta in array
---
+++ Java Script/Arrays/Method in array/unShift.js
@@ -0,0 +1,6 @@
+// unShift is used  add value in array in first index
+
+let arr=['tony', 'hulk', 'blackvido'];
+console.log(arr);// befor using unshift output
+arr.unshift('krish')
+console.log(arr);// affter using unshift output
