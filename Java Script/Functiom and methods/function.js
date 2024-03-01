4b1fa87503f1f491d0800c2178ad857c05f6c4dd
ichanddan <code.us.cm@gmail.com>
2024-03-01 23:44:10 +0530

    sum two value using function and return

--- Java Script/Functiom and methods/function.js
+++ Java Script/Functiom and methods/function.js
@@ -1,8 +1,18 @@
-function myFunction(){
-    console.log('Hello World');
-}
-myFunction
+// function myFunction(){
+//     console.log('Hello World');
+// }
+// myFunction
 
-function badla(msg){ // aurgument (using this is peramiter)
-    console.log(msg) //paeramiter
+// function badla(msg){ // aurgument (using this is peramiter)
+//     console.log(msg) //paeramiter
+// }
+function sum(a,b){
+    console.log(a+b)
 }
+sum(5,19)
+function letsum(a,b){
+    b=a+b
+    return b
+}
+let value=letsum(10,34)
+console.log(value)
