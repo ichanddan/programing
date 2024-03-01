93e12e671762ff86ff575be16626fbf218c9942e
ichanddan <code.us.cm@gmail.com>
2024-03-01 23:54:43 +0530

    Arrow function : multiply two value suing arrow function and return

---
+++ Java Script/Functiom and methods/Arrow function.js
@@ -0,0 +1,17 @@
+// sign of arrow function is => 
+const arrowsum= (a,b)=>{
+    console.log(a+b)
+}
+arrowsum(5,5);
+
+//  mutiply two valu using arrow function
+const arrowmul= (a,b)=>{
+    console.log(a*b)
+}
+arrowmul(5,5);
+const arrowmu1= (a,b)=>{
+    return a *b
+}
+arrowmul(5,5);
+
+// the below code fragment can be found in:
