79b36a2e1e15cf5eaddf4934e0a71b404fb1189c
ichanddan <code.us.cm@gmail.com>
2024-03-02 22:28:00 +0530

    Square of array value using forEach function

---
+++ Java Script/Functiom and methods/Q2.js
@@ -0,0 +1,9 @@
+// for a given of number print the square of each value using forEach loop
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+arr.forEach(function (value ,index) {
+  console.log(value * value, index);
+});
+
+// the below code fragment can be found in:
