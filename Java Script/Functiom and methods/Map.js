e7785eae93323459f2df8fe52938cdbd92dbf3c1
ichanddan <code.us.cm@gmail.com>
2024-03-02 23:20:16 +0530

    Map method using arrow function

---
+++ Java Script/Functiom and methods/Map.js
@@ -0,0 +1,10 @@
+// map array methods
+
+
+let array = [10,34,65,76,32,343];
+let new1 = array.map((val)=>{
+    // console.log(value)
+    return val*val; // return is a crete a new value
+});
+console.log(array); // this is a old value 
+console.log(new1); // this is a new value
--- Java Script/Functiom and methods/Q2.js
+++ Java Script/Functiom and methods/Q2.js
@@ -1,9 +1,18 @@
 // for a given of number print the square of each value using forEach loop
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-arr.forEach(function (value ,index) {
-  console.log(value * value, index);
+// arr.forEach(function (value ,index) {
+//   console.log(value * value, index);
+// });
+
+// secound method
+let pow=[22.45,667,7877,5545467]
+
+let main=(function (view) {
+  console.log(view * view);
 });
+pow.forEach(main)
+
 
 // the below code fragment can be found in:
