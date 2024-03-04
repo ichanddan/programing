0a78e545c5900382549a81895f95033cf758149e
ichanddan <code.us.cm@gmail.com>
2024-03-04 23:45:54 +0530

    DOM

---
+++ Java Script/DOM/script.js
@@ -0,0 +1,6 @@
+// document.getElementById('id');
+// document.getElementsByClassName('clasName');
+// document.getElementsByTagName('tageName')
+// alter net
+// document.querySelector("id",'className', 'tage Selector'); select fist element from html
+// document.querySelectorAll("id",'className', 'tage Selector'); select all element for html 
