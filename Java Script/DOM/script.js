2a4b3c0287bc48e9a3224c74ee65b9ff43735a7f
ichanddan <code.us.cm@gmail.com>
2024-03-05 00:16:55 +0530

    DOM Manipulation: read

--- Java Script/DOM/script.js
+++ Java Script/DOM/script.js
@@ -4,3 +4,21 @@
 // alter net
 // document.querySelector("id",'className', 'tage Selector'); select fist element from html
 // document.querySelectorAll("id",'className', 'tage Selector'); select all element for html 
+
+
+// --------Dom Manipulation--------
+
+// tageName: return tage for element Node
+
+
+// innerText: return the text content of the element and all childern 
+// example
+// let example= document.querySelector('div').innerText
+// console.log(example) // show all text inside of div
+
+
+// innerHtml: return all tag like a text
+// let example= document.querySelector('body').innerHTML
+// console.log(example) // show all text inside of html
+
+// textContent : return textyal content even for hide Element
