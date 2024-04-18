933b66ce7d50b69026cd68dfdbdd59b47e4aee17
ichanddan <code.us.cm@gmail.com>
2024-04-19 01:38:35 +0530

    Connect Database with mangoDB

---
+++ Node/chai aur backend/src/index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:'.env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express'
+// const app=express()
+// ( async ()=>{
+//     try {
+//        mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//        app.on("error", (error)=>{
+//         Console.log("ERRR: ",error);
+//         throw error
+//        })
+//        app.listen(process.env.PORT, ()=>{
+//         console.log(`Your server is run ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+//         console.error("ERROR: ",error)
+//     }
+// })()
